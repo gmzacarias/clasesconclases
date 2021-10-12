6383c7a897cec0bde1a7956e3a57f45fa227c0b2
Gaston Mauro Zacarias <gastonmzacarias@gmail.com>
2021-10-12 03:39:36 -0300

    commit pc uno

---
+++ index.ts
@@ -0,0 +1,50 @@
+// las clases siempre se llaman en singular,salvo excepciones
+class BandaAlbum {
+    title : string;
+    songs : string[];
+    // title : string; aca solo se define un string
+    // songs : string[]; aca se define un array de strings
+constructor (title ,songs : string[]){
+    this.title=title;
+    this.songs=songs;
+}
+}
+
+
+class banda {
+members : string[];
+albums:any[];
+constructor(members:string[],albums:BandaAlbum[])
+{
+    this.members = members;
+    this.albums = albums;
+}
+
+}
+
+
+
+//
+function main (){
+// opcion 1
+// const unAlbum = new BandaAlbum("album de rock", [])
+// opcion 2
+const unAlbum = {
+    title:"detonador de sueños",
+    songs :["hiela sangre","detonador de sueños","la razon que te demora"],
+}
+const unAlbum2 = {
+    title : "nonpalidece",
+songs : ["dangerman","espejos","en el aire"],
+}
+const unAlbum3 = {
+    title : "los cafres",
+    songs: ["tus ojos", "velas y sahumerios", "bastara"],
+}
+
+const unaBanda = new banda (["gaston"], [unAlbum,unAlbum2,unAlbum3])
+// console.log (unaBanda.members);
+console.log(unaBanda.albums);
+
+}
+main();
---
+++ indexdesafio.ts
@@ -0,0 +1,81 @@
+// la funcion this sirve para leer y guardar
+// crear las clases Edificio, Piso y Departamento aquí
+class Departamento{
+    nombre:string;
+    constructor(nombre){
+this.nombre=nombre;
+    }
+    getName(){
+return this.nombre;
+    }
+}
+
+
+//
+class Piso{
+    nombre: string;
+    // = [] le dice a departamento que se inicialize con un array vacio
+    deptos : Departamento[] = [];
+
+    constructor(nombre){
+this.nombre= nombre;
+
+    }
+    pushDepartamento(depto:Departamento){
+     return   this.deptos.push(depto);
+    }
+    getDepartamentos(): Departamento[]{
+    return    this.deptos;
+
+    }
+}
+//
+class Edificio{
+    pisos :Piso[]
+    constructor(pisos: Piso[]){
+this.pisos = pisos;
+    }
+addDepartamentoToPiso(nombreDePiso:string , departamento: Departamento){
+const pisoEncontrado = this.pisos.find((p)=> p.nombre == nombreDePiso);
+return pisoEncontrado.pushDepartamento(departamento);
+}
+// ejemplo
+// getDepartamentoByPiso(nombreDelPiso:string): Departamento[]{} ,en este ejemplo : sirve para definir que tipo de respuesta/return queremos
+// en este caso seria un array de la clase departamentos
+getDepartamentosByPiso(nombreDelPiso:string):Departamento[] {
+    const pisoEncontrado = this.pisos.find((p)=> p.nombre == nombreDelPiso);
+    return pisoEncontrado.getDepartamentos();
+}
+
+};
+// no modificar este test
+function testClaseEdificio() {
+    const unPiso = new Piso("planta baja");
+    const otroPiso = new Piso("primer piso");
+    const unEdificio = new Edificio([unPiso, otroPiso]);
+    const deptoUno = new Departamento("depto uno");
+    const deptoDos = new Departamento("depto dos");
+    const deptoTres = new Departamento("depto tres");
+    unEdificio.addDepartamentoToPiso("planta baja", deptoUno);
+    unEdificio.addDepartamentoToPiso("planta baja", deptoDos);
+    unEdificio.addDepartamentoToPiso("planta baja", deptoTres);
+  
+    const deptos = unEdificio.getDepartamentosByPiso("planta baja");
+    const deptosEmpty = unEdificio.getDepartamentosByPiso("primer piso");
+  
+    if (
+      Array.isArray(deptosEmpty) &&
+      deptosEmpty.length == 0 &&
+      deptos.length == 3 &&
+      deptos[2].getName() == "depto tres"
+    ) {
+      console.log("testClaseBandaApartment passed");
+    } else {
+      throw "testClaseBandaApartment not passed";
+    }
+  }
+  
+  function main() {
+    testClaseEdificio();
+  }
+  main();
